8d67fa04b2031b4e71f46539c9a3faf5e1444263
Fredrik Roos <fredrik@stensatter.se>
2026-02-14 12:06:11 +0100

    tests

--- src/test/java/com/crazedout/ronah/RonahTest.java
+++ src/test/java/com/crazedout/ronah/RonahTest.java
@@ -1,10 +1,12 @@
 package com.crazedout.ronah;
 
 import com.crazedout.ronah.api.APIService;
+import com.crazedout.ronah.service.HttpRequest;
 import com.crazedout.ronah.service.RonahHttpServer;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
@@ -14,6 +16,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RonahTest {
 
     int port = 8083;
+    String connect(String http) throws IOException {
+        return connect(http,null,null);
+    }
+    String connect(String http, String type, String payload) throws IOException {
+        try(Socket s = new Socket("localhost",port)) {
+            PrintStream ps = new PrintStream(s.getOutputStream());
+            if(payload==null) {
+                ps.println(http + "\n");
+            }else{
+                ps.println(http);
+                ps.println("Content-Type: " + type);
+                ps.println("Content-Length: " + payload.length() + "\n");
+                ps.print(payload);
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                    // read headers.
+                }
+                return reader.readLine();
+            }
+        }
+    }
 
     @Test
     public void test1() throws Exception {
@@ -26,18 +51,25 @@ public class RonahTest {
         t.start();
 
         Thread.sleep(500);
-        try(Socket s = new Socket("localhost",port)) {
-            PrintStream ps = new PrintStream(s.getOutputStream());
-            ps.println("GET /index HTTP/1.1\n\n");
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null && !line.isEmpty()) {
-                    // read headers.
-                }
-                assertEquals("OK", reader.readLine());
-            }
-        }
+        String res = connect("GET /index HTTP/1.1");
+        assertEquals("OK", res);
+
+        res = connect("GET /param?name=ronah&age=1 HTTP/1.1");
+        assertEquals("ronah=1", res);
+
     }
 
+    @Test
+    public void test2() throws IOException{
+        String res = connect("GET /param?name=ronah&age=1 HTTP/1.1");
+        assertEquals("ronah=1", res);
+    }
+
+    @Test
+    public void test3() throws IOException{
+        String json = "{\"name\":\"ringo\",\"band\":\"the beatles\"}";
+        String res = connect("POST /post HTTP/1.1", HttpRequest.APPLICATION_JSON,json);
+        assertEquals("ringo plays in the beatles",res);
+    }
     // TODO: More tests...
 }
--- src/test/java/com/crazedout/ronah/TestService.java
+++ src/test/java/com/crazedout/ronah/TestService.java
@@ -1,8 +1,11 @@
 package com.crazedout.ronah;
 
 import com.crazedout.ronah.annotation.GET;
+import com.crazedout.ronah.annotation.POST;
+import com.crazedout.ronah.annotation.Param;
 import com.crazedout.ronah.service.AutoRegisterService;
 import com.crazedout.ronah.service.Request;
+import org.json.JSONObject;
 
 public class TestService extends AutoRegisterService {
 
@@ -10,5 +13,16 @@ public class TestService extends AutoRegisterService {
     public static void test1(Request request){
         request.getResponse().ok("OK").send();
     }
+
+    @GET(path="/param", response = "text/text")
+    public static void test2(Request request, @Param String name, @Param String age) {
+        request.getResponse().ok(name+"="+age).send();
+    }
+
+    @POST(path="/post", response = "text/text")
+    public static void test3(Request request, @Param JSONObject json) {
+        request.getResponse().ok(json.get("name") + " plays in " + json.get("band")).send();
+    }
+
 
 }
