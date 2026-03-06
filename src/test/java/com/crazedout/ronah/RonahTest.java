860e1de6a5c5bd0982b7346aa6fd58a0c477fe56
Fredrik Roos <fredrik@stensatter.se>
2026-03-06 17:34:12 +0100

    test fix

--- src/test/java/com/crazedout/ronah/RonahTest.java
+++ src/test/java/com/crazedout/ronah/RonahTest.java
@@ -112,12 +112,10 @@ public class RonahTest extends TestUtils {
     void testWebServer() throws IOException {
         TestWebServer s = new TestWebServer("src/test");
         res = connect("GET /web HTTP/1.1");
-        //assertEquals("</html>",res.get(res.size()-1));
-        res.forEach(System.out::println);
+        assertEquals("OK",res.get(res.size()-1));
         res = connect("GET /web/index.html HTTP/1.1");
-        res.forEach(System.out::println);
         Repository.removeService(s);
-        //assertEquals("</html>",res.get(res.size()-1));
+        assertEquals("OK",res.get(res.size()-1));
     }
 
     // TODO: More tests...
